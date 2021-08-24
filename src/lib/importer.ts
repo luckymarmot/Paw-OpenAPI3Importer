fa2ba6f3eb2af358ed5247fd1c8a74afed284260
Pongstr <twiz.tickler@gmail.com>
2021-08-24 14:04:45 +0300

    improve(validation-prompt): validation message
    - improved: validation error message
    - updated : bumped swagger-parser version to 10.0.3

--- src/lib/importer.ts
+++ src/lib/importer.ts
@@ -1,9 +1,10 @@
 import Yaml from 'yaml'
-import SwaggerParser from '@apidevtools/swagger-parser'
+import SwaggerParser, { resolve } from '@apidevtools/swagger-parser'
 import { OpenAPIV3 } from 'openapi-types'
 import Paw from 'types/paw'
 import PawConverter from './converter'
 import config from '../paw.config'
+import { logger } from 'utils'
 
 const { identifier, title, inputs, fileExtensions } = config
 
@@ -42,7 +43,9 @@ export default class OpenAPIv3Importer implements Paw.Importer {
   public canImport(context: Paw.Context, items: Paw.ExtensionItem[]): number {
     return items.reduce((acc, item: Paw.ExtensionItem) => {
       const doc = this.parseContent(item) as OpenAPIV3.Document
-      if (!doc) return 0
+
+      if (!doc || !doc.openapi) return 0
+
       return (
         doc.openapi.substr(0, 1) === '3.0' && // allowed versions 3.0.x.*
         typeof doc.info === 'object' &&
@@ -71,14 +74,20 @@ export default class OpenAPIv3Importer implements Paw.Importer {
     items: Paw.ExtensionItem[],
     options: Paw.ExtensionOption,
   ): Promise<boolean> {
-    const documents = [...items].map(
-      (item: Paw.ExtensionItem): Promise<OpenAPIV3.Document> => {
+    const documents = items.map((item: Paw.ExtensionItem) => {
+      const doc = this.parseContent(item)
+      return new Promise((resolve, reject) => {
+        if (Object.prototype.hasOwnProperty.call(doc, 'name')) {
+          reject(new Error(`${doc.name}\n ${doc.message}.`))
+          return
+        }
+
         const apiParser = new SwaggerParser()
-        const apiDocument = this.parseContent(item)
         const filename =
           item.file?.name.replace(/\.(yml|yaml|json)$/, '') || item.name
-        return apiParser
-          .validate(apiDocument, this.parserOptions)
+
+        apiParser
+          .validate(doc, this.parserOptions)
           .then(() => {
             const convertDocument = new PawConverter(
               apiParser,
@@ -86,11 +95,11 @@ export default class OpenAPIv3Importer implements Paw.Importer {
               context,
             )
             convertDocument.init()
-            return apiParser.api
+            resolve(apiParser.api)
           })
-          .catch((error) => error)
-      },
-    )
+          .catch((error) => reject(error))
+      })
+    })
 
     return Promise.all(documents)
       .then((data) => data.length > 0)
@@ -109,18 +118,15 @@ export default class OpenAPIv3Importer implements Paw.Importer {
    * @param {String} opts.content
    * @returns {Object<OpenAPIV3.Document>}
    */
-  private parseContent({
-    mimeType,
-    content,
-  }: Paw.ExtensionItem): OpenAPIV3.Document {
+  private parseContent({ mimeType, content }: Paw.ExtensionItem): any {
     try {
       const context =
         mimeType === 'application/json'
           ? JSON.parse(content)
-          : Yaml.parse(content)
+          : Yaml.parse(content, { prettyErrors: true })
       return context
     } catch (error) {
-      throw new Error(`Failed to parse OpenAPI file: ${error}`)
+      return error
     }
   }
 }
